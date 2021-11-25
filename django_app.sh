215f408522b45dca57545c02311b0a2497574160
dumbo25 <dumbo25@users.noreply.github.com>
2021-11-25 08:42:17 -0600

    Update django_app.sh

--- django_app.sh
+++ django_app.sh
@@ -1,4 +1,34 @@
-# Create a separate script for Django Apps:
+#!/bin/bash
+# sript to setup one django app
+#
+# run using
+#    sudo bash [bash_options] django_app.sh [install_options]
+
+############################# <- 80 Characters -> ##############################
+#
+# This style of commenting is called a garden bed and it is frowned upon by
+# those who know better. But, I like it. I use a garden bed at the top to track
+# my to do list as I work on the code, and then mark the completed To Dos.
+#
+# Within the code, To Dos include a comment with ???, which makes it easy to
+# find
+#
+# Once the To Do list is more-or-less complete, I move the completed items into
+# comments, docstrings or help within the code. Next, I remove the completed
+# items.
+#
+# Create a deploy script (move to a new scipt)
+#   a) deploy script
+#      a.1) security checklist
+#      a.2) turn off Django debug
+#      a.3) run this: manage.py check --deploy
+#      a.4) use systemd to start webserver
+#           generate myproject.services file systemd and load and start service at boot
+#
+# Create a seperate script to add css (move to a new script)
+#   a) add ui css
+#
+# To Do List:
 #   a) create a shell script for apps
 #      a.1) need to add urls.py to each app
 #       nano /home/pi/newt_site/urls.py or is it newt_site/newt_site
@@ -16,14 +46,20 @@
 #      c.2) do a hello world static page, instead of rocketship
 #      c.3) ensure static files work
 #      c.4) turn off django debug
-#   d) deploy script
-#      d.1) security checklist
-#      d.2) turn off Django debug
-#      d.3) run this: manage.py check --deploy
+		# the app script can add multiple apps
+		# must enable static pages
+		# must create yellow world page
+		# must work in both venv and no-venv
 #
-# Create a seperate script to add css
-#   a) add ui css
+#   w) tesr all options
+#   x) install from scratch 
+#   y) run shellccheck
+#   z) check into github
 #
-#   i) use systemd to start webserver
-#      i.1) generate myproject.services file systemd and load and start service at boot
-#   k) move the additonal script comments above elsewhere
+# Do later or not at all:
+#
+# References:
+#
+############################# <- 80 Characters -> ##############################
+
+################################## Functions ###################################
