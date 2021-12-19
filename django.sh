0c249bb07eedb7eb87307e6a08299e1799a5e1ce
dumbo25 <dumbo25@users.noreply.github.com>
2021-12-19 08:21:01 -0600

    19DEC2021

--- django.sh
+++ django.sh
@@ -17,7 +17,45 @@
 # comments, docstrings or help within the code. Next, I remove the completed
 # items.
 #
+# Create a separate script for Django Apps:
+#   a) create a shell script for apps
+#      a.1) need to add urls.py to each app
+#       nano /home/pi/newt_site/urls.py or is it newt_site/newt_site
+#      a.2) app.urls
+#      a.3) get beyond rocket ship django demo page
+#   b) use django apps checklist
+#   c) get static pages to work 
+#      c.1) https://djangoforbeginners.com/hello-world/)
+#           better than above: https://learndjango.com/tutorials/hello-world-5-different-ways
+#           python manage.py startapp pages
+#           # nano pages/views.py
+#           from django.http import HttpResponse
+#           def homePageView(request):
+#               return HttpResponse("Hello, World!")
+#      c.2) do a hello world static page, instead of rocketship
+#      c.3) ensure static files work
+#      c.4) turn off django debug
+#   d) deploy script
+#      d.1) security checklist
+#      d.2) turn off Django debug
+#      d.3) run this: manage.py check --deploy
+#
+# Create a seperate script to add css
+#   a) add ui css
+#
+#   i) use systemd to start webserver
+#      i.1) generate myproject.services file systemd and load and start service at boot
+#   k) move the additonal script comments above elsewhere
+#
 # To Do List:
+# ??? **************************** STOPPED HERE ******************** ???
+#
+#   x) fix issues in logs
+#      x.1) tail /var/log/apache2/access.log
+#      x.2) tail /var/log/apache2/error.log
+#      x.3) cat /var/log/syslog
+#   y) run shellcheck
+#   z) add install.sh and install.cfg to github 
 #
 # Do later or not at all:
 #   l) create checkVersion function; read in version number and command to get version number ??? python3, pip3, django
